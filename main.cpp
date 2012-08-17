fc81b14b45a3b66939884cc0e9ad67628daf4f82
mic448 <mic448@mic448-AMILO-Pi-3540>
2012-08-18 00:39:38 +0200

    Pridany int

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: mic448
+ *
+ * Created on 18. srpen 2012, 0:22
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
